07d1ec3a8211a70e27a3b7a8a5dd5a1ed65fa537
matsu-chara <matsuy00@gmail.com>
2016-01-20 16:01:34 +0900

    Initial commit

---
+++ example_projects/trait-example/build.sbt
@@ -0,0 +1,6 @@
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "org.scalikejdbc" %% "scalikejdbc" % "2.3.4",
+  "org.mindrot"     %  "jbcrypt"     % "0.3m"
+)
---
+++ example_projects/trait-refactored-example/src/main/scala/domain/UserService.scala
@@ -0,0 +1,30 @@
+package domain
+
+trait UserService {
+  self: PasswordService with UserRepository =>
+
+  val maxNameLength = 32
+
+  def register(name: String, rawPassword: String): User = {
+    if (name.length > maxNameLength) {
+      throw new Exception("Too long name!")
+    }
+    if (find(name).isDefined) {
+      throw new Exception("Already registered!")
+    }
+    insert(User(name, hashPassword(rawPassword)))
+  }
+
+  def login(name: String, rawPassword: String): User = {
+    find(name) match {
+      case None       => throw new Exception("User not found!")
+      case Some(user) =>
+        if (!checkPassword(rawPassword, user.hashedPassword)) {
+          throw new Exception("Invalid password!")
+        }
+        user
+    }
+  }
+}
+
+class UserServiceImpl extends UserService with PasswordServiceImpl with UserRepositoryImpl
