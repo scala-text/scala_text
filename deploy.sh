eefdad9f2fb2705598a1eab2055ccdcedd9ad99e
xuwei-k <6b656e6a69@gmail.com>
2018-02-13 13:22:45 +0900

    必要ない && を削除
    先頭で `set -e` してるので、必要ないはず

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -ex
 
-echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config &&
-openssl aes-256-cbc -k "$SERVER_KEY" -in travis_deploy_key.enc -d -a -out deploy.key &&
-cp deploy.key ~/.ssh/ &&
-chmod 600 ~/.ssh/deploy.key &&
-git config --global user.email "6b656e6a69@gmail.com" &&
-git config --global user.name "xuwei-k" &&
-mv gitbook/_book ../ &&
-mv gitbook/scala_text.epub ../_book/scala_text.epub &&
-git fetch origin gh-pages:gh-pages &&
-git clean -fdx &&
-git checkout gh-pages &&
-rm -rf ./* &&
-echo -e "*class\ntarget" > .gitignore &&
-cp -r ../_book/* ./ &&
-git add . &&
-git commit -a -m "auto commit on travis $TRAVIS_JOB_NUMBER $TRAVIS_COMMIT" &&
+echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
+openssl aes-256-cbc -k "$SERVER_KEY" -in travis_deploy_key.enc -d -a -out deploy.key
+cp deploy.key ~/.ssh/
+chmod 600 ~/.ssh/deploy.key
+git config --global user.email "6b656e6a69@gmail.com"
+git config --global user.name "xuwei-k"
+mv gitbook/_book ../
+mv gitbook/scala_text.epub ../_book/scala_text.epub
+git fetch origin gh-pages:gh-pages
+git clean -fdx
+git checkout gh-pages
+rm -rf ./*
+echo -e "*class\ntarget" > .gitignore
+cp -r ../_book/* ./
+git add .
+git commit -a -m "auto commit on travis $TRAVIS_JOB_NUMBER $TRAVIS_COMMIT"
 if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_EVENT_TYPE}" == "push" && "${TRAVIS_REPO_SLUG}" == "dwango/scala_text"  ]];
 then git push git@github.com:dwango/scala_text.git gh-pages:gh-pages ; fi
 
