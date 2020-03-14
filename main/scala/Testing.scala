3ebd00e9ade34bdb6ed56f1086268a61d50c3fc1
Kota Mizushima <mizukota@gmail.com>
2020-03-14 08:22:08 +0000

    auto commit on travis 1130.1 da418adc293b15278de771c93d5b8524e00aed68

--- main/scala/Testing.scala
+++
@@ -1,7 +0,0 @@
-import org.scalacheck._, Arbitrary.arbitrary
-object Testing {
-  def test[G](g: Gen[G])(f: G  => Boolean) = {
-    val result = Prop.forAll(g)(f).apply(Gen.Parameters.default)
-    assert(result.success, result)
-  }
-}
